2039ea0aaceb83ca7c643eef62fc4222e5f5914b
rybakouvlad <rybakou.vladik@gmail.com>
2021-05-08 20:14:52 +0300

    feat: add all task

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # caesar-cli
+
+Run task:
+
+1. npm install
+2. node my_caesar_cli --action encode --shift 7 --input input.txt --output.txt
+
+CLI tool should accept 4 options (short alias and full name):
+
+-s, --shift: a shift
+-i, --input: an input file
+-o, --output: an output file
+-a, --action: an action encode/decode
--- src/caesar-cli.js
+++ src/caesar-cli.js
@@ -1,7 +1,7 @@
 const { Command } = require("commander");
 const program = new Command();
-
-console.log("kuku");
+const { EventsHandling } = require("./stream");
+const eventsHandling = new EventsHandling();
 
 program
   .option("-s, --shift <number>", "a shift")
@@ -11,4 +11,64 @@ program
 
 const options = program.parse();
 
-console.log(options.opts());
+async function run() {
+  let action = true;
+
+  if (!options.opts().shift || !options.opts().action) {
+    console.error("Incorrect action or shift");
+    process.exit(1);
+  }
+
+  if (options.opts().action === "encode") {
+    action = true;
+  } else if (options.opts().action === "decode") {
+    action = false;
+  } else {
+    console.error("Incorrect action");
+    process.exit(1);
+  }
+
+  if (options.opts().input && options.opts().output) {
+    if (eventsHandling.checkFile(options.opts().input)) {
+      return;
+    }
+
+    try {
+      await eventsHandling.writeFullData(
+        options.opts().input,
+        options.opts().output,
+        options.opts().shift,
+        action
+      );
+    } catch (error) {
+      console.error(error);
+    }
+    process.exit(0);
+  }
+  if (options.opts().output) {
+    try {
+      await eventsHandling.inputStream(
+        options.opts().output,
+        options.opts().shift,
+        action
+      );
+    } catch (error) {}
+    return;
+  }
+  if (options.opts().input) {
+    if (eventsHandling.checkFile(options.opts().input)) {
+      return;
+    }
+    try {
+      await eventsHandling.readStream(
+        options.opts().input,
+        options.opts().shift,
+        action
+      );
+    } catch (error) {}
+    return;
+  }
+  eventsHandling.consoleStream(options.opts().shift, action);
+}
+
+run();
